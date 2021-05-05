49a0c350d17119f2cbcad0fcfddf62f059d0fbfe
Neeraj Dalal <nd941z@gmail.com>
2021-05-05 22:06:49 +0530

    Wed May  5 16:36:49 UTC 2021

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # The world's largest crypto exchange trading library
 
-Not available for public usage yet.
+[![GitHub license][mit-image]][mit-url] &middot;
+[![NPM Version][npm-image]][npm-url] &middot;
+[![Downloads Stats][npm-downloads]][npm-url] &middot;
+
+[mit-image]: https://img.shields.io/badge/license-MIT-blue.svg
+[mit-url]: https://en.wikipedia.org/wiki/MIT_License
+[npm-image]: https://img.shields.io/npm/v/binancefy.svg?style=flat
+[npm-downloads]: https://img.shields.io/npm/dm/binancefy.svg?style=flat
+[npm-url]: https://npmjs.org/package/binancefy
+
+> Status: Under Development
+
+## Installation
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/).
+
+Before installing, [download and install Node.js](https://nodejs.org/en/download/).
+Node.js 0.10 or higher is required.
+
+If this is a brand new project, make sure to create a `package.json` first with
+the [`npm init` command](https://docs.npmjs.com/creating-a-package-json-file).
+
+Installation is done using the
+[`npm install` command](https://docs.npmjs.com/getting-started/installing-npm-packages-locally):
+
+```sh
+$ npm i binancefy
+```
+
+## Usage
+
+Get trading symbols by their Quote
+
+```js
+const cc = require('binancefy')
+
+const getSymbols = async () => {
+	const symbols = cc.quote('USDT')
+	console.log(symbols)
+}
+
+getSymbols()
+```
+
+## Examples
+
+To be updated soon.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ actions/cc.js
@@ -0,0 +1,83 @@
+const axios = require('axios')
+
+const markets = require('./markets.json')
+
+const base = async (base, baseVolume = 0) => {
+	base = base.toUpperCase()
+
+	const filter = []
+
+	for (element of [...markets.fiat]) {
+		element = base + element
+		filter.push(element)
+	}
+
+	const res = await axios.get('https://api.binance.com/api/v3/ticker/24hr')
+
+	const resData = []
+
+	for (element of res.data) {
+		if (element.symbol.startsWith(base) && element.volume > baseVolume) {
+			let flag = 0
+
+			for (x of filter) {
+				if (element.symbol === x) {
+					flag = 1
+					break
+				}
+			}
+
+			if (flag) {
+				resData.push({
+					symbol: element.symbol,
+					baseVolume: element.volume,
+				})
+			}
+		}
+	}
+
+	symbols = resData.sort((a, b) => b.baseVolume - a.baseVolume)
+
+	return symbols
+}
+
+const quote = async (quote, quoteVolume = 0) => {
+	quote = quote.toUpperCase()
+
+	const filter = []
+
+	for (element of [...markets.fiat, ...markets.etf]) {
+		element = element + quote
+		filter.push(element)
+	}
+
+	const res = await axios.get('https://api.binance.com/api/v3/ticker/24hr')
+
+	const resData = []
+
+	for (element of res.data) {
+		if (element.symbol.endsWith(quote) && element.quoteVolume > quoteVolume) {
+			let flag = 1
+
+			for (x of filter) {
+				if (element.symbol.endsWith(x)) {
+					flag = 0
+					break
+				}
+			}
+
+			if (flag) {
+				resData.push({
+					symbol: element.symbol,
+					quoteVolume: element.quoteVolume,
+				})
+			}
+		}
+	}
+
+	symbols = resData.sort((a, b) => b.quoteVolume - a.quoteVolume)
+
+	return symbols
+}
+
+module.exports = { base, quote }
